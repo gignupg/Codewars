6853e60849529248b3b72d2d5c41b1c110cf935b
gignu <linus.komnick@googlemail.com>
2020-11-18 22:00:31 +0100

    I'm at my wits end. I'll read up on permutations online and then solve it probably using recursion

--- kyu 4/Permutations/my-solution.js
+++ kyu 4/Permutations/my-solution.js
@@ -1,33 +1,99 @@
-function permutations(string) {
-    const n = string.length;
-    let letterPerPosition = 1;
-    const oneArr = [];
+function fillingArrWithChars(string, n, charsPerPos) {
     const fullArr = [];
-
-    if (n === 1) return [string];
-
-    // Calculate the amount of times a letter re-appears at the same position
-    for (let i = 2; i < n; i++) {
-        letterPerPosition *= i;
-    }
+    const preparationArr = [];
 
     for (let i = 0; i < n; i++) {
-        oneArr.push({letter: string[i], amount: letterPerPosition});
+        preparationArr.push({ letter: string[i], amount: charsPerPos });
     }
-
     for (let i = 0; i < n; i++) {
-        fullArr.push(oneArr);
+        fullArr.push(JSON.parse(JSON.stringify(preparationArr)));
     }
-
 
     return fullArr;
 }
 
+function generateAllCombinations(fullArr, n, charsPerPos) {
+    const resultArr = [];
+    const totalChars = n * charsPerPos;
+
+    for (let i = 0; i < totalChars; i++) {
+        let newStrCombo = "";
+        const usedCharPos = [];
+
+        if (i >= 0) {
+            console.log("||| New i |||", i)
+            console.log("Snapshot:");
+            console.log(fullArr);
+            console.log("Array so far:");
+            console.log(resultArr);
+        }
+
+        // Looping through each character position
+        for (let j = 0; j < n; j++) {
+            const arr = fullArr[j];
+            const score = [];       // Give each obj a score, then push the obj with the highest score!
+
+            // Looping through each possible character for each position (j), then choosing one character.
+            for (let k = 0; k < n; k++) {
+
+                if (usedCharPos.includes(k)) {
+                    score.push(0);
+
+                } else {
+                    score.push(arr[k].amount);
+                }
+            };
+
+            // Find the next letter
+            if (score.length) {
+                const highestScore = [...score].sort((a, b) => b - a)[0];
+                const highScorePos = score.indexOf(highestScore);
+                const obj = arr[highScorePos];
+
+                // if (i >= 0 && i <= 1) {
+                //     console.log("score", score)
+                //     console.log("highestScore", highestScore)
+                //     console.log("highScorePos", highScorePos)
+                //     console.log("obj", obj)
+                //     console.log("-----------------------------")
+                // }
+
+                newStrCombo += obj.letter;
+                obj.amount--;
+                usedCharPos.push(highScorePos);
+            }
+        };
+        resultArr.push(newStrCombo);
+    }
+    return resultArr;
+}
+
+
+function permutations(string) {
+    const n = string.length;
+
+    if (n === 1) return [string];
+
+    // Calculate the amount of times a letter re-appears at the same position
+    let charsPerPos = 1;
+    for (let i = 2; i < n; i++) {
+        charsPerPos *= i;
+    }
+
+    const fullArr = fillingArrWithChars(string, n, charsPerPos);
+    const resultArr = generateAllCombinations(fullArr, n, charsPerPos);
+
+    // Getting rid of duplicates before returning. 
+    return [...new Set(resultArr)];
+}
+
 // console.log(permutations('ab'));
 // ['ab', 'ba']
-console.log(permutations('abc'));
-// ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-// console.log(permutations('aabb'));;
+// permutations('abc');
+// ['abc', 'bac'...]
+permutations('abcd');
+// ['abcd', 'bacd'...]
+// console.log(permutations('aabb'));
 // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 
 
@@ -35,16 +101,16 @@ console.log(permutations('abc'));
 
 // Choosing the right structure:
 // [
-//   [{letter: "a", amount: 2}, {letter: "b", amount: 2}, {letter: "c", amount: 2}], 
+//   [{letter: "a", amount: 2}, {letter: "b", amount: 2}, {letter: "c", amount: 2}],
 //   [{letter: "a", amount: 2}, {letter: "b", amount: 2}, {letter: "c", amount: 2}],
 //   [{letter: "a", amount: 2}, {letter: "b", amount: 2}, {letter: "c", amount: 2}]
 // ]
 
 // Full Object
-// {1: [[a,a],[b,b],[c,c]], 2: [[a,a],[b,b],[c,c]], 3: [[a,a],[b,b],[c,c]]}
-// {1: [[a],[b,b],[c,c]], 2: [[a,a],[b],[c,c]], 3: [[a,a],[b,b],[c]]}
-// {1: [[a],[b],[c,c]], 2: [[a],[b],[c,c]], 3: [[a,a],[b,b]]}
-// {1: [[a],[b],[c]], 2: [[b],[c,c]], 3: [[a,a],[b]]}
+// {1: [a,a,b,b,c,c], 2: [a,a,b,b,c,c], 3: [a,a,b,b,c,c]}
+// {1: [a,b,b,c,c], 2: [a,a,b,c,c], 3: [a,a,b,b,c]}
+// {1: [a,b,c,c], 2: [a,b,c,c], 3: [a,a,b,b]}
+// {1: [a,b,c], 2: [b,c,c], 3: [a,a,b]}
 // {1: [b,c], 2: [b,c], 3: [a,a]}
 // {1: [c], 2: [b], 3: [a]}
 // {1: [], 2: [], 3: []}
@@ -58,12 +124,9 @@ console.log(permutations('abc'));
 // {1: [a,b,c,a,b], 2: [b,a,a,c,c], 3: [c,c,b,b,a]}
 // {1: [a,b,c,a,b,c], 2: [b,a,a,c,c,b], 3: [c,c,b,b,a,a]}
 
-// a, b, c
-// b, a, c
-// c, a, b
-// a, c, b
-// b, c, a
-// c, b, a
+
+// a b c d
+// 
 
 
 // Let's see if there is a pattern: The same thing but with 4
@@ -74,71 +137,20 @@ console.log(permutations('abc'));
 // Empty Object
 // {1: [], 2: [], 3: [], 4:}
 
-// a b c d
-// a b d c
-// a c b d
-// a c d b
-// a d b c
-// a d c b
 
-// b a c d
-// b a d c
-// b c a d
-// b c d a
-// b d a c
-// b d c a
-
-// c a b d
-// c a d b
-// c b a d
-// c b d a
-// c d a b
-// c d b a
-
-// d a b c
-// d a c b
-// d b a c
-// d b c a
-// d c a b
-// d c b a
-
-
-// With 5:
-
-// e a b c d
-// e a b d c
-// e a c b d
-// e a c d b
-// e a d b c
-// e a d c b
-
-// e b a c d
-// e b a d c
-// e b c a d
-// e b c d a
-// e b d a c
-// e b d c a
-
-// e c a b d
-// e c a d b
-// e c b a d
-// e c b d a
-// e c d a b
-// e c d b a
-
-// e d a b c
-// e d a c b
-// e d b a c
-// e d b c a
-// e d c a b
-// e d c b a
-
+// 2 - 6 -24 -120
 // 1 - 2 - 6 - 24
-// 2 - 3 - 4 - 5
+// 2 - 3 - 4 -  5
 
 // Task: 4 different letters how many times can each letter be in position 1?
 
 // for loop pass in 4 as max length:
 
 // for (let i = 2; i <= 4; i++) {
-// 
+
+// abcd
+// abdc
+// acbd
+// acdb
+// adbc
+// adcb
